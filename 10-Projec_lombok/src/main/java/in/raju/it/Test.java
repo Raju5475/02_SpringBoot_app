ba2ebfae9f0ade5a3d2cf5c98cfc55be72d304bb
Raju5475 <rajraju7687@gmail.com>
2023-04-05 13:45:02 -0400

    raju

---
+++ 10-Projec_lombok/src/main/java/in/raju/it/Test.java
@@ -0,0 +1,8 @@
+package in.raju.it;
+
+public class Test {
+	public static void main(String[] args) {
+		User u=new User();
+	}
+
+}
---
+++ 10-Projec_lombok/src/main/java/in/raju/it/User.java
@@ -0,0 +1,17 @@
+package in.raju.it;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+
+public class User {
+	private int pIds;
+	private int phno;
+	private String name;
+	private int Long;
+
+
+}
